4fa032a571a2312449ed84ef20df864ad98a33a8
RioMahler <229354130+RioMahler@users.noreply.github.com>
2025-10-15 12:19:38 +0200

    Add UI

---
+++ script.js
@@ -0,0 +1,13 @@
+let buttons = document.querySelectorAll("button");
+
+buttons.forEach((button) => button.addEventListener("click", operate));
+
+function operate() {}
+
+function add() {}
+
+function subtract() {}
+
+function mulitply() {}
+
+function divide() {}
